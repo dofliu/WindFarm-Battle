faa100600caf8fe4da066dab0d9778dc6351c930
dofliu <9.0171766e+07+dofliu@users.noreply.github.com>
2026-05-30 19:55:11 +0000

    Updates

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: [
+    'dist',
+    'node_modules',
+    '*.cjs',
+    '*.config.js',
+    'vite.config.ts',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+  },
+};
